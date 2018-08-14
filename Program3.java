78715d3a4f4c219338f3e63f735d5fe7bf3b1a1e
rithuchandran <chandranrithu@gmail.com>
2018-08-14 20:02:56 +0530

    third program

---
+++ Program3.java
@@ -0,0 +1,9 @@
+import org.jetbrains.annotations.NotNull;
+
+public class Program3 {
+    public static void main (@NotNull String[] args){
+        int n=Integer.parseInt(args[0]);
+        for (int i = 0; i <n ; i++)
+            System.out.println("*");
+    }
+}
