ea6dbdaa8fe6e78122b132ad33d294f347515c45
rithuchandran <chandranrithu@gmail.com>
2018-08-14 19:44:30 +0530

    second program

---
+++ Program2.java
@@ -0,0 +1,11 @@
+import org.jetbrains.annotations.NotNull;
+
+public class Program2 {
+public static void main(@NotNull String[] args)
+{
+    int n=Integer.parseInt(args[0]);
+    for(int i=0;i<n;i++)
+        System.out.print("*");
+
+}
+}
